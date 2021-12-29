7b2a219ff5dc2940a4d023f9bf5a66b9432055ef
Abhishek Chaudhary <73344265+acadroit@users.noreply.github.com>
2021-12-29 12:24:23 +0530

    Create dannys_diner.sql

---
+++ case_study_1/dannys_diner.sql
@@ -0,0 +1,5 @@
+/*
+What is the total amount each customer spent at the restaurant?
+*/
+select s.customer_id, sum(m.price) from sales s join menu m on s.product_id=m.product_id group by s.customer_id
+
