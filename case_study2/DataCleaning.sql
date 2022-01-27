7045c84044aaf5fff1afb5d82200b08069fa1ef0
Abhishek Chaudhary <73344265+acadroit@users.noreply.github.com>
2022-01-27 23:04:11 +0530

    Update DataCleaning.sql

--- case_study2/DataCleaning.sql
+++ case_study2/DataCleaning.sql
@@ -1,15 +1,39 @@
 /* cleaning customer_orders table */
 
-create view cleaned_customer_orders as(
-select order_id,customer_id,pizza_id, 
-case 
-	when exclusions='null' then ' '
-	else exclusions
-	end as exclusions,
-case 
-	when extras is null or extras='null' then ' '
+CREATE VIEW cleaned_customer_orders AS(
+SELECT order_id,customer_id,pizza_id, 
+CASE 
+	WHEN exclusions='null' then ' '
+	ELSE exclusions
+	END AS exclusions,
+CASE 
+	WHEN extras is null or extras='null' then ' '
 	else extras
-	end as extras,
+	END AS extras,
 	order_time
-from customer_orders)
-select * from cleaned_customer_orders
+FROM customer_orders)
+SELECT * FROM cleaned_customer_orders
+
+/* cleaning runner_orders table */
+
+
+CREATE VIEW runner_orders_cleaned AS(
+SELECT order_id, runner_id,
+  CASE 
+    WHEN pickup_time LIKE 'null' THEN ' '
+    ELSE pickup_time 
+    END AS pickup_time,
+  CASE 
+    WHEN distance LIKE 'null' THEN ' '
+    WHEN distance LIKE '%km' THEN TRIM('km' from distance) 
+    ELSE distance END AS distance,
+  CASE 
+    WHEN duration LIKE 'null' THEN ' ' 
+    WHEN duration LIKE '%mins' THEN TRIM('mins' from duration) 
+    WHEN duration LIKE '%minute' THEN TRIM('minute' from duration)        
+    WHEN duration LIKE '%minutes' THEN TRIM('minutes' from duration)       
+    ELSE duration END AS duration,
+  CASE 
+    WHEN cancellation IS NULL or cancellation LIKE 'null' THEN ''
+    ELSE cancellation END AS cancellation
+FROM runner_orders)
